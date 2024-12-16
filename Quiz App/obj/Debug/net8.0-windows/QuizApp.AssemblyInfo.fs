357b19e6b6f38fafffdb2ed012c825b1ddc07898
Ahmed Mohammed Sayed <123858042+Ahmed72-AMSA@users.noreply.github.com>
2024-12-16 08:13:02 +0200

    Final_touch_upgrade_commit

--- Quiz App/obj/Debug/net8.0-windows/QuizApp.AssemblyInfo.fs
+++ Quiz App/obj/Debug/net8.0-windows/QuizApp.AssemblyInfo.fs
@@ -10,7 +10,11 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("QuizApp")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
+<<<<<<< Updated upstream
 [<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71e7bd79dd20e7c18ae27322f961cf6d05f3ae5f")>]
+=======
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85c25487b068f0b33c2cfe9853d4cdea3eaebfff")>]
+>>>>>>> Stashed changes
 [<assembly: System.Reflection.AssemblyProductAttribute("QuizApp")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("QuizApp")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
